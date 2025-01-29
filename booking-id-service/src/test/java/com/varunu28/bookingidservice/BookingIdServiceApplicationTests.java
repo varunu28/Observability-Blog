9a0a2e942f77762197f564d95d991a8b4dcaa2ce
varunu28 <varun.u28@gmail.com>
2025-01-29 08:04:45 -0800

    Initialized booking-id-service & externalized docker component

---
+++ booking-id-service/src/test/java/com/varunu28/bookingidservice/BookingIdServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.varunu28.bookingidservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingIdServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
